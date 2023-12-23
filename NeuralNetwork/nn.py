50be4ef8484973b06ed26d5bff0f0edf7dadcc27
Arturo Cepeda <arturocepedaperez@gmail.com>
2023-12-23 13:52:19 +0100

    [NeuralNetwork] Loading weights and biases when creating the network, if available

--- NeuralNetwork/nn.py
+++ NeuralNetwork/nn.py
@@ -1,6 +1,9 @@
 
 import json
 import keras
+import numpy
+import os.path
+import pandas
 
 from keras.models import Sequential
 from keras.layers import Dense
@@ -32,4 +35,31 @@ def createNetwork(name):
   network.add(Dense(hiddenLayerSize, input_dim=inputLayerSize, activation=kHiddenLayerActivation, kernel_initializer=kKernelInitializer))
   network.add(Dense(outputLayerSize, activation=kHiddenLayerActivation, kernel_initializer=kKernelInitializer))
   
+  # Load the weights, if available
+  for layerIndex in range(len(network.layers)):
+    weightsNP = numpy.empty(network.layers[layerIndex].get_weights()[0].shape)
+    weightsFilePath = name + "/_layer" + str(layerIndex) + "_weights_"
+
+    if os.path.exists(weightsFilePath):
+      csvData = pandas.read_csv(weightsFilePath, sep=" ", header=None)
+      layerWeightsLength = len(network.layers[layerIndex].get_weights()[0])
+      layerWeights = csvData.iloc[:,:layerWeightsLength].values
+      
+      for x in range(layerWeightsLength):
+        for y in range(len(network.layers[layerIndex].get_weights()[0][x])):
+          weightsNP[x][y] = layerWeights[x][y]
+
+    biasNP = numpy.empty(network.layers[layerIndex].get_weights()[1].shape)
+    biasesFilePath = name + "/_layer" + str(layerIndex) + "_bias_"
+
+    if os.path.exists(biasesFilePath):
+      csvData = pandas.read_csv(biasesFilePath, sep=" ", header=None)
+      layerBiasLength = len(network.layers[layerIndex].get_weights()[1])
+      layerBias = csvData.iloc[:,:layerBiasLength].values
+    
+      for x in range(layerBiasLength):
+        biasNP[x] = layerBias[0][x]
+      
+    network.layers[layerIndex].set_weights(numpy.array([weightsNP, biasNP], dtype=object))
+
   return network
